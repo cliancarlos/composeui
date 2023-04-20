3324e505d0b03d99a52c1eec33083945362252e5
Clian Carlos <clian.dev@outlook.com>
2023-04-20 16:46:38 -0300

    fix: clean classes in Text variants

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { createBaseComponent } from '../../utils/createBaseComponent';
 
 const textVariants = [
-  { variant: 'h1', classes: 'text-5xl font-bold', element: 'h1' },
-  { variant: 'h2', classes: 'text-4xl font-medium', element: 'h2' },
-  { variant: 'h3', classes: 'text-3xl italic', element: 'h3' },
-  { variant: 'h4', classes: 'text-2xl text-sm', element: 'h4' },
-  { variant: 'h5', classes: 'text-xl font-bold', element: 'h5' },
-  { variant: 'h6', classes: 'text-lg font-medium text-sm', element: 'h6' },
-  { variant: 'label', classes: 'text-base text-blue-600', element: 'label' },
-  { variant: 'default', classes: 'text-base text-blue-600', element: 'p' },
+  { variant: 'h1', classes: '', element: 'h1' },
+  { variant: 'h2', classes: '', element: 'h2' },
+  { variant: 'h3', classes: '', element: 'h3' },
+  { variant: 'h4', classes: '', element: 'h4' },
+  { variant: 'h5', classes: '', element: 'h5' },
+  { variant: 'h6', classes: '', element: 'h6' },
+  { variant: 'label', classes: '', element: 'label' },
+  { variant: 'default', classes: '', element: 'span' },
 ];
 
 const Text = createBaseComponent(textVariants);
